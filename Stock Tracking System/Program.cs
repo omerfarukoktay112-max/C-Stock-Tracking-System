fcc782067f455c105ed401a6650a14ab1afca700
Ömer Faruk Oktay <omerfarukoktay112@gmail.com>
2025-11-12 20:05:25 +0300

    feat: Initial commit of CSharp Stock Tracking Sys

---
+++ Stock Tracking System/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Tracking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> productNames = new List<string>();
+            List<int> productQuantities = new List<int>();
+            List<double> productPrices = new List<double>();
+
+            while (true)
+            {
+                Console.WriteLine("Stock Tracking System");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Update Product");
+                Console.WriteLine("3. Delete Product");
+                Console.WriteLine("4. List Products");
+                Console.WriteLine("5. Total Stock Value");
+                Console.WriteLine("6. Exit");
+                Console.Write("Select an option (1-6): ");
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Enter product name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Enter quantity: ");
+                    int quantity = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter price: ");
+                    double price = Convert.ToDouble(Console.ReadLine());
+
+                    productNames.Add(name);
+                    productQuantities.Add(quantity);
+                    productPrices.Add(price);
+
+                    Console.WriteLine("Product added successfully!\n");
+                }
+                else if (choice == "2")
+                {
+                    Console.Write("Enter product name to update: ");
+                    string name = Console.ReadLine();
+
+                    int index = productNames.IndexOf(name);
+                    if (index != -1)
+                    {
+                        Console.Write("Enter new quantity: ");
+                        productQuantities[index] = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter new price: ");
+                        productPrices[index] = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Product updated successfully!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product not found!\n");
+                    }
+                }
+                else if (choice == "3")
+                {
+                    Console.Write("Enter product name to delete: ");
+                    string name = Console.ReadLine();
+
+                    int index = productNames.IndexOf(name);
+                    if (index != -1)
+                    {
+                        productNames.RemoveAt(index);
+                        productQuantities.RemoveAt(index);
+                        productPrices.RemoveAt(index);
+
+                        Console.WriteLine("Product deleted successfully!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product not found!\n");
+                    }
+                }
+                else if (choice == "4")
+                {
+                    Console.WriteLine("Product List:");
+                    Console.WriteLine("-------------");
+                    for (int i = 0; i < productNames.Count; i++)
+                    {
+                        double totalValue = productQuantities[i] * productPrices[i];
+                        Console.WriteLine($"{productNames[i]} - {productQuantities[i]} units - {productPrices[i]} TL - Total: {totalValue} TL");
+                    }
+                    Console.WriteLine();
+                }
+                else if (choice == "5")
+                {
+                    double totalStockValue = 0;
+                    for (int i = 0; i < productNames.Count; i++)
+                    {
+                        totalStockValue += productQuantities[i] * productPrices[i];
+                    }
+                    Console.WriteLine("Total stock value: " + totalStockValue + " TL\n");
+                }
+                else if (choice == "6")
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option! Please try again.\n");
+                }
+            }
+        }
+    }
+}
---
+++ Stock Tracking System/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Stock Tracking System")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stock Tracking System")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("41e0c24c-a7af-454c-8c60-0403a7830c26")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
